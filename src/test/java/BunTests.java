828520306e5e002d7dfb4beaeebfc8352e163b46
Vasily Romanov <romanovve@yandex.ru>
2025-06-27 22:43:07 +0300

    первое задание диплом

---
+++ src/test/java/BunTests.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import praktikum.Bun;
+
+import static org.junit.Assert.assertEquals;
+
+public class BunTests {
+
+    /**
+     * Проверка метода getName: проверяет, что название булочки возвращается корректно.
+     */
+    @Test
+    public void testGetName() {
+        Bun bun = new Bun("black bun", 100.0f);
+        assertEquals("Название булочки некорректно", "black bun", bun.getName());
+    }
+
+    /**
+     * Проверка метода getPrice: проверяет, что цена булочки возвращается корректно.
+     */
+    @Test
+    public void testGetPrice() {
+        Bun bun = new Bun("black bun", 100.0f);
+        assertEquals("Цена булочки некорректна", 100.0f, bun.getPrice(), 0.0f);
+    }
+}
